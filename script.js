5c7097edbb646798755d5815481bd4a8d6664ef3
mayzintheintone <mayzintheint.one@gmail.com>
2025-12-30 01:12:30 +0630

    Learning Datatype

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 console.log("Hello World!");
 prompt("This is sum of 3 and 4");
 console.log(3 + 4);
+let num = 4;
+let sum = 1;
+if (true) {
+  if (sum >= 0) {
+    const x = 5;
+    addition = x + num;
+  }
+}
+const PI = 3.142;
+console.log(PI);
