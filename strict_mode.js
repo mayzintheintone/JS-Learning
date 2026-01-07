5fb21bd35d6d5d3caa6755f58497abf3fe6dea64
mayzintheintone <mayzintheint.one@gmail.com>
2026-01-07 23:02:30 +0630

    Strict Mode

---
+++ strict_mode.js
@@ -0,0 +1,10 @@
+// VI. Strict Mode
+// write the string "use strict " at the ever top of js file
+"use strict";
+
+let isAdult = true;
+let letRegisteration;
+isAdult ? (letRegiteration = true) : (letRegisteration = false);
+console.log(letRegisteration);
+// num = 10; //missing let or const keyword to declare  a variable
+// console.log(num);
