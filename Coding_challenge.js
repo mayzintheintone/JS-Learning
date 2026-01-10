171eeccdfbf5098778afbac2c5a3117acbaa63af
mayzintheintone <mayzintheint.one@gmail.com>
2026-01-10 23:42:26 +0630

    BMI coding

--- Coding_challenge.js
+++ Coding_challenge.js
@@ -1,35 +1,35 @@
 "use strict";
 // Data 1
-let massofJohn = 92;
-let massofMark = 78;
-let heightofJohn = 1.95;
-let heightofMark = 1.69;
-let bmiofJohn = massofJohn / heightofJohn ** 2;
-let bmiofMark = massofMark / heightofMark ** 2;
-let markHigherBMI = bmiofJohn > bmiofMark;
-console.log(bmiofJohn);
-console.log(bmiofMark);
+let massOfJohn = 92;
+let massOfMark = 78;
+let heightOfJohn = 1.95;
+let heightOfMark = 1.69;
+let bmiOfJohn = massOfJohn / heightOfJohn ** 2;
+let bmiOfMark = massOfMark / heightOfMark ** 2;
+let markHigherBMI = bmiOfJohn < bmiOfMark;
+console.log(bmiOfJohn);
+console.log(bmiOfMark);
 console.log(markHigherBMI);
 
 // Data 2
-let massofJohn1 = 85;
-let massofMark1 = 95;
-let heightofJohn1 = 1.76;
-let heightofMark1 = 1.88;
-let bmiofJohn1 = massofJohn1 / heightofJohn1 ** 2;
-let bmiofMark1 = massofMark1 / heightofMark1 ** 2;
-let markHigherBMI1 = bmiofJohn1 > bmiofMark1;
-console.log(bmiofJohn1);
-console.log(bmiofMark1);
+let massOfJohn1 = 85;
+let massOfMark1 = 95;
+let heightOfJohn1 = 1.76;
+let heightOfMark1 = 1.88;
+let bmiOfJohn1 = massOfJohn1 / heightOfJohn1 ** 2;
+let bmiOfMark1 = massOfMark1 / heightOfMark1 ** 2;
+let markHigherBMI1 = bmiOfJohn1 < bmiOfMark1;
+console.log(bmiOfJohn1);
+console.log(bmiOfMark1);
 console.log(markHigherBMI1);
 
 // Using Function to calculate BMI
-function BMI(mass, height) {
+function haveBMI(mass, height) {
   return mass / (height * height);
 }
-let bmiofJohn2 = BMI(92, 1.95);
-let bmiofMark2 = BMI(78, 1.69);
-let markHigherBMI2 = bmiofJohn2 > bmiofMark2;
-console.log(`BMI of John is ${bmiofJohn2}`);
-console.log(`BMI of Mark is ${bmiofMark2}`);
+let bmiOfJohn2 = haveBMI(92, 1.95);
+let bmiOfMark2 = haveBMI(78, 1.69);
+let markHigherBMI2 = bmiOfJohn2 < bmiOfMark2;
+console.log(`BMI of John is ${bmiOfJohn2}`);
+console.log(`BMI of Mark is ${bmiOfMark2}`);
 console.log(markHigherBMI2);
